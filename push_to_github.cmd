c77333a0e3b5c2b7e74ec525c7795f0d5ac55dfa
Sandeep Yadav <sandeepengineering03@gmail.com>
2026-08-12 22:48:48 +0530

    Add GitHub push script

---
+++ push_to_github.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo ===================================================
+echo Pushing Public Madadgar Consultancy to GitHub
+echo Account: https://github.com/SandeepSDE-eng
+echo ===================================================
+git remote set-url origin https://github.com/SandeepSDE-eng/public-madadgar-consultancy.git
+git push -u origin main
+pause
